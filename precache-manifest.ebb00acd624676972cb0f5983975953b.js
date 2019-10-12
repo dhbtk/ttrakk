17676fc750e96bc2baa8dd9e58bc034c34f4ee83
Eduardo Antonio Niehues <eduardo@edanni.io>
2019-10-11 21:38:40 -0300

    Updates

---
+++ precache-manifest.ebb00acd624676972cb0f5983975953b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c92044c2a5d786e9bb08b9b61b2cea2",
+    "url": "/ttrakk/index.html"
+  },
+  {
+    "revision": "5862d4195a9ac1438085",
+    "url": "/ttrakk/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "d026e90bacb335a56b73",
+    "url": "/ttrakk/static/css/main.9fd6bbbd.chunk.css"
+  },
+  {
+    "revision": "5862d4195a9ac1438085",
+    "url": "/ttrakk/static/js/2.e83a5c90.chunk.js"
+  },
+  {
+    "revision": "d026e90bacb335a56b73",
+    "url": "/ttrakk/static/js/main.ec7e6c3c.chunk.js"
+  },
+  {
+    "revision": "766bcf8f2f7d7325e1be",
+    "url": "/ttrakk/static/js/runtime-main.c61ff8da.js"
+  }
+]);
